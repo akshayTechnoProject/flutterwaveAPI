f044b36ec34069f923d2a52881d1b8c57f411069
akshayTechnoProject <akshaymalaviya4545@gmail.com>
2022-06-15 10:33:56 +0530

    .

--- src/component/transaction.jsx
+++ src/component/transaction.jsx
@@ -1,55 +1,81 @@
-import React, { useEffect } from 'react';
+import React from 'react';
+import axios from 'axios';
 
 export default function transaction() {
-  const getData = () => {
-    fetch('https://api.flutterwave.com/v3/transfers', {
-      //mode: 'no-cors',
-      method: 'GET',
-      headers: {
-        Authorization: 'Bearer FLWSECK_TEST-f76c049ba3f5791f1f47f734ece7d305-X',
-        'Access-Control-Allow-Origin': 'https://localhost:3000',
-
-        //'content-type': 'application/json',
-        //accept: 'application/json',
-      },
-      //body: JSON.stringify({
-      //name: this.state.name,
-      //notes: this.state.notes,
-      //}),
-    })
-      .then((response) => response.text())
-      .then((response) => {
-        console.log(response);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-    /*
-        const myurl = 'https://trulipay.herokuapp.com/api/admin/login';
-        var bodyFormData = new URLSearchParams();
-        bodyFormData.append('auth_code', 'TruliPay#Wallet$&$aPp#MD');
-        axios({
-          method: 'post',
-          url: myurl,
-          data: bodyFormData,
-          headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-        })
-          .then((response) => {
-            console.log('Errors', response.data);
-            if (response.data.success) {
-              //navigate(`/otp-check`, { state: email });
-            } else {
-              //setDisable(false);
-              //toast.error('!...Your email or password is wrong');
-            }
-          })
-          .catch((error) => {
-            console.log('Errors', error);
-            //toast.error('!...Something went wrong');
-            //setDisable(false);
-          });
-        */
+  var requestOptions = {
+    method: 'GET',
+    redirect: 'follow',
+    headers: {
+      Authorization: 'Bearer FLWSECK_TEST-f76c049ba3f5791f1f47f734ece7d305-X',
+      'Access-Control-Allow-Origin': 'http://localhost:3000',
+      'Access-Control-Allow-Methods': 'GET',
+    },
   };
-  getData();
+  fetch(
+    'https://api.flutterwave.com/v3/banks/NG?public_key=Bearer FLWPUBK_TEST-b4b9e29ad77d0e0f23a26500a6bb9ba3-X',
+    requestOptions
+  )
+    .then((response) => response.text())
+    .then((result) => console.log(result))
+    .catch((error) => console.log('error', error));
+
+  /*
+  const getData = () => {
+    var myHeaders = new Headers();
+    myHeaders.append(
+      'Authorization',
+      'Bearer FLWPUBK_TEST-b4b9e29ad77d0e0f23a26500a6bb9ba3-X'
+    );
+    myHeaders.append('Access-Control-Allow-Origin', '*');
+*/
+  //myHeaders.append('Content-Type', 'application/json');
+  //myHeaders.append('Content-Type', 'application/json');
+
+  /*var raw = JSON.stringify({
+      account_bank: '044',
+      account_number: '0690000040',
+      amount: 5500,
+      narration: 'Akhlm Pstmn Trnsfr xx007',
+      currency: 'NGN',
+      reference: 'akhlm-pstmnpyt-rfxx007_PMCKDU_1',
+      callback_url: 'https://webhook.site/d35869f4-b63d-4d3f-8a8e-12a47a2381b2',
+      debit_currency: 'NGN',
+    });
+
+    var requestOptions = {
+      method: 'GET',
+      headers: myHeaders,
+      //body: '',
+      //redirect: 'follow',
+    };
+
+    fetch('https://api.flutterwave.com/v3/banks/NG', requestOptions)
+      .then((response) => response.text())
+      .then((result) => console.log(result))
+      .catch((error) => console.log('error', error));
+    */
+  /*
+    const myurl = 'https://api.flutterwave.com/v3/banks/NG';
+    var bodyFormData = new URLSearchParams();
+    //bodyFormData.append('auth_code', 'TruliPay#Wallet$&$aPp#MD');
+    axios({
+      method: 'GET',
+      url: myurl,
+      headers: myHeaders,
+      redirect: 'follow',
+      //data: bodyFormData,
+      // headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+    })
+      .then((response) => {
+        console.log('...', response.data);
+        if (response.data.success) {
+        } else {
+        }
+      })
+      .catch((error) => {
+        console.log('Errorsss', error);
+      });
+  };*/
+  //getData();
   return <div>transaction</div>;
 }
